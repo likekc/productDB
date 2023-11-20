98ee8a281bd390a3105936276f509867246e7b92
liikkc <mikigm100@gmail.com>
2023-11-20 23:31:46 +0900

    DBup1

--- mongo/mon2.js
+++ mongo/mon2.js
@@ -17,13 +17,16 @@ async function updateDataInDB(collectionName, newData) {
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
-    // 기존 데이터 업데이트
+    // 기존 데이터 업데이트 또는 새로운 데이터 추가
     for (const data of newData) {
-      await collection.updateOne({ id: data.id }, { $set: { price: data.price, quantity: data.quantity } });
+      const filter = { id: data.id };
+      const update = { $set: { price: data.price, quantity: data.quantity } };
+      const options = { upsert: true }; // upsert 옵션을 통해 문서가 없으면 새로 생성
+      await collection.updateOne(filter, update, options);
     }
 
-// 업데이트된 DB 리스트 출력
-const updatedDocuments = await collection.find().toArray();
+    // 업데이트된 DB 리스트 출력
+    const updatedDocuments = await collection.find().toArray();
     console.log(`Updated documents in ${collectionName} collection:`, updatedDocuments);
   } finally {
     // 연결 종료
